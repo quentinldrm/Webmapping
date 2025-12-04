00e391347e43d7dc928e70be0d08bb966f3e462c
Quentin Ledermann <quentinledermann@outlook.fr>
2025-12-04 15:29:56 +0100

    RAS

--- comparaison/script-compare.js
+++ comparaison/script-compare.js
@@ -76,7 +76,7 @@ function initParkings(data) {
         pointToLayer: (feature, latlng) => {
             const parkingIcon = L.divIcon({
                 className: 'custom-parking-icon',
-                html: '<div class="parking-marker-symbol">P</div>',
+                html: '<div class="parking-marker-symbol">P+R</div>',
                 iconSize: [24, 24], iconAnchor: [12, 12]
             });
             return L.marker(latlng, { icon: parkingIcon, pane: 'zParkings' });
