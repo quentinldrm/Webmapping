03b63c2ae317980fcf7b9ec5a4e28427e3c3f010
Quentin Ledermann <quentinledermann@outlook.fr>
2025-12-04 15:11:21 +0100

    Style modification

--- access/script-access.js
+++ access/script-access.js
@@ -313,13 +313,28 @@ function initFiltersEvents() {
 
     // B. Checkbox Réseau Global
     const toggleNet = document.getElementById('toggle-network');
-    if(toggleNet) {
+
+    if (toggleNet && networkData) {
         toggleNet.addEventListener('change', () => {
             layers.background.clearLayers();
+            
             if (toggleNet.checked) {
-                L.geoJSON(data.lines, {
-                    style: { color: "#888", weight: 1, opacity: 0.3, dashArray: '3, 6' },
-                    pane: 'zBackground', interactive: false 
+                L.geoJSON(networkData, {
+                    style: function(feature) {
+                        
+                        const type = feature.properties.route;
+
+                        switch (type) {
+                            case 'tram':
+                                return { color: "#4cc9f0", weight: 2, opacity: 0.3 };
+                            case 'bus':
+                                return { color: "#ff9f1c", weight: 1, opacity: 0.3, dashArray: '3, 6' };
+                            default:
+                                return { color: "#888", weight: 1, opacity: 0.3, dashArray: '3, 6' };
+                        }
+                    },
+                    pane: 'zBackground', 
+                    interactive: false 
                 }).addTo(layers.background);
             }
         });
